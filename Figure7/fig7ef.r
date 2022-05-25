0fd78da090f53f1cba86936476c7fa42d996b8bd
yiliao1022 <34287238+yiliao1022@users.noreply.github.com>
2022-05-24 17:49:33 -0700

    Update fig7ef.r

--- Figure7/fig7ef.r
+++ Figure7/fig7ef.r
@@ -66,5 +66,106 @@ wilcox.test(Density~Types, data = mydata3_filter,  alternative ="less", paired =
 
 
 
+### For Loop tau
+setwd("/home/yiliao/Documents/Pepper_2021/Final/6_Figure_loop/FINAL/TADvsLoop")
+mydata2 <-read.table("Figure6.Final.tau.loop.tissues.bed.bed",head=T)
+head(mydata2)
+mydata2$Tissues<- as.factor(mydata2$Tissues)
+ggplot(mydata2, aes(x=Tissues, y=Tau, fill=factor(Types))) + geom_boxplot(outlier.shape = NA) + ylim(0,1) +labs(fill = "Types")
+
+mydata2_filter <-mydata2 %>% filter(Tissues=='All') %>% filter(Types=='Specific' | Types=='Shared')
+wilcox.test(Tau~Types, data = mydata2_filter,  alternative ="less", paired = FALSE)
+mydata2_specific <-mydata2 %>% filter(Tissues=='All') %>% filter(Types=='Specific')
+mydata2_Shared <-mydata2 %>% filter(Tissues=='All') %>% filter(Types=='Shared')
+lengths (mydata2_specific)
+lengths (mydata2_Shared)
+
+
+mydata2_filter <-mydata2 %>% filter(Tissues=='GR') %>% filter(Types=='Specific' | Types=='Shared')
+wilcox.test(Tau~Types, data = mydata2_filter,  alternative ="less", paired = FALSE)
+mydata2_specific <-mydata2 %>% filter(Tissues=='GR') %>% filter(Types=='Specific')
+mydata2_Shared <-mydata2 %>% filter(Tissues=='GR') %>% filter(Types=='Shared')
+lengths (mydata2_specific)
+lengths (mydata2_Shared)
+
+
+mydata2_filter <-mydata2 %>% filter(Tissues=='HL') %>% filter(Types=='Specific' | Types=='Shared')
+wilcox.test(Tau~Types, data = mydata2_filter,  alternative ="less", paired = FALSE)
+mydata2_specific <-mydata2 %>% filter(Tissues=='HL') %>% filter(Types=='Specific')
+mydata2_Shared <-mydata2 %>% filter(Tissues=='HL') %>% filter(Types=='Shared')
+lengths (mydata2_specific)
+lengths (mydata2_Shared)
+
+
+mydata2_filter <-mydata2 %>% filter(Tissues=='TZ') %>% filter(Types=='Specific' | Types=='Shared')
+wilcox.test(Tau~Types, data = mydata2_filter, alternative ="less", paired = FALSE)
+mydata2_specific <-mydata2 %>% filter(Tissues=='TZ') %>% filter(Types=='Specific')
+mydata2_Shared <-mydata2 %>% filter(Tissues=='TZ') %>% filter(Types=='Shared')
+lengths (mydata2_specific)
+lengths (mydata2_Shared)
+
+
+mydata2_filter <-mydata2 %>% filter(Tissues=='YP') %>% filter(Types=='Specific' | Types=='Shared')
+wilcox.test(Tau~Types, data = mydata2_filter, alternative ="less", paired = FALSE)
+mydata2_specific <-mydata2 %>% filter(Tissues=='YP') %>% filter(Types=='Specific')
+mydata2_Shared <-mydata2 %>% filter(Tissues=='YP') %>% filter(Types=='Shared')
+lengths (mydata2_specific)
+lengths (mydata2_Shared)
+
+####################### For Loop Fold change 
+
+mydata3 <-read.table("/home/yiliao/Documents/Pepper_2021/Final/6_Figure_loop/20210902/Boundaries.loop.fold.out.out",head=T)
+
+mydata3$Tissues <- as.factor(mydata3$Tissues )
+ggplot(mydata3, aes(x=Tissues, y=Density, fill=factor(Types))) + geom_boxplot(outlier.shape = NA) + ylim(-1,15)+labs(fill = "Types") +
+  labs(title="absolute log2FC for loops based HiCexplorer output", x= "Types", y= "abs(log2FC)")
+
+mydata3_filter <-mydata3 %>% filter(Tissues=='HLvsGR')  %>% filter(Types=='shared' | Types=='speci')
+wilcox.test(Density~Types, data = mydata3_filter, alternative ="less", paired = FALSE) 
+mydata3_specific <-mydata3 %>% filter(Tissues=='HLvsGR') %>% filter(Types=='speci')
+mydata3_Shared <-mydata3 %>% filter(Tissues=='HLvsGR') %>% filter(Types=='shared')
+lengths (mydata3_specific)
+lengths (mydata3_Shared)
+
+mydata3_filter <-mydata3 %>% filter(Tissues=='HLvsTZ')  %>% filter(Types=='shared' | Types=='speci')
+wilcox.test(Density~Types, data = mydata3_filter, alternative ="less", paired = FALSE) 
+mydata3_specific <-mydata3 %>% filter(Tissues=='HLvsTZ') %>% filter(Types=='speci')
+mydata3_Shared <-mydata3 %>% filter(Tissues=='HLvsTZ') %>% filter(Types=='shared')
+lengths (mydata3_specific)
+lengths (mydata3_Shared)
+
+mydata3_filter <-mydata3 %>% filter(Tissues=='TZvsGR')  %>% filter(Types=='shared' | Types=='speci')
+wilcox.test(Density~Types, data = mydata3_filter, alternative ="less", paired = FALSE) 
+mydata3_specific <-mydata3 %>% filter(Tissues=='TZvsGR') %>% filter(Types=='speci')
+mydata3_Shared <-mydata3 %>% filter(Tissues=='TZvsGR') %>% filter(Types=='shared')
+lengths (mydata3_specific)
+lengths (mydata3_Shared)
+
+mydata3_filter <-mydata3 %>% filter(Tissues=='YPvsGR')  %>% filter(Types=='shared' | Types=='speci')
+wilcox.test(Density~Types, data = mydata3_filter,  alternative ="less", paired = FALSE) 
+mydata3_specific <-mydata3 %>% filter(Tissues=='YPvsGR') %>% filter(Types=='speci')
+mydata3_Shared <-mydata3 %>% filter(Tissues=='YPvsGR') %>% filter(Types=='shared')
+lengths (mydata3_specific)
+lengths (mydata3_Shared)
+
+mydata3_filter <-mydata3 %>% filter(Tissues=='YPvsHL')  %>% filter(Types=='shared' | Types=='speci')
+wilcox.test(Density~Types, data = mydata3_filter, alternative ="less", paired = FALSE) 
+mydata3_specific <-mydata3 %>% filter(Tissues=='YPvsHL') %>% filter(Types=='speci')
+mydata3_Shared <-mydata3 %>% filter(Tissues=='YPvsHL') %>% filter(Types=='shared')
+lengths (mydata3_specific)
+lengths (mydata3_Shared)
+
+mydata3_filter <-mydata3 %>% filter(Tissues=='YPvsTZ')  %>% filter(Types=='shared' | Types=='speci')
+wilcox.test(Density~Types, data = mydata3_filter,  alternative ="less", paired = FALSE) 
+mydata3_specific <-mydata3 %>% filter(Tissues=='YPvsTZ') %>% filter(Types=='speci')
+mydata3_Shared <-mydata3 %>% filter(Tissues=='YPvsTZ') %>% filter(Types=='shared')
+lengths (mydata3_specific)
+lengths (mydata3_Shared)
+
+
+
+
+
+
 
 
